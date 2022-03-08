7d05e92749b8c768cf8b5270c1f66a6f2cfd04e1
Abhi250692 <abhishekjawajwar25@gmail.com>
2022-03-08 20:20:29 +0000

    Adding shell script for android SDK installtion

---
+++ install_sdk.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# yum install android-tools -y
+cd /root/
+yum install java-1.8.0-openjdk-devel
+mkdir -p android-sdk-linux
+cd android-sdk-linux
+# @see https://developer.android.com/studio/index.html
+wget --output-document=android-sdk.zip --quiet https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
+unzip android-sdk.zip
+rm -f android-sdk.zip
+yes | tools/bin/sdkmanager --licenses
+export ANDROID_HOME=$root/android-sdk-linux
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/platform-toolsvim /etc/profile.d/gradle.sh
